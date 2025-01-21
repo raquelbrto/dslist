0864ddc3c6bb531d3998af5b4856cb586de75cd5
RaquelABrito <raquel.brito.059@ufrn.edu.br>
2025-01-21 20:20:57 -0300

    feat: create game controller

---
+++ src/main/java/com/dev/dslist/controllers/GameController.java
@@ -0,0 +1,23 @@
+package com.dev.dslist.controllers;
+
+import com.dev.dslist.entities.Game;
+import com.dev.dslist.services.GameService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping(value = "/games")
+public class GameController {
+
+    @Autowired
+    private GameService gameService;
+
+    @GetMapping
+    public List<Game> findAllGames() {
+        return gameService.findAll();
+    }
+}
